a0e8f49e1d81f5288b989226fc5d3a09c6d096ff
gurkasahan <gurk.asahan@gmail.com>
2024-12-01 15:00:02 -0500

    Working on Controller Classes

--- FlexiFit/Controllers/ClassesController.cs
+++ FlexiFit/Controllers/ClassesController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// FlexiFit/Controllers/ClassesController.cs
+using Microsoft.AspNetCore.Mvc;
+using FlexiFit.Entities;
+using FlexiFit.Services;
+using System.Linq;
+using FlexiFit.Entities.Models;
 
 namespace FlexiFit.Controllers
 {
+    /// <summary>
+    /// Author: Alfred, Gurkaranjit, Kamaldeep
+    /// Manages class-related operations.
+    /// </summary>
+    [Route("[controller]/[action]")]
     public class ClassesController : Controller
     {
-        public IActionResult Index()
+        private readonly IRepository<Class> _classRepository;
+
+        public ClassesController(IRepository<Class> classRepository)
         {
-            return View();
+            _classRepository = classRepository;
         }
+
+        // GET: Classes/List
+        [HttpGet]
+        public IActionResult List()
+        {
+            var classes = _classRepository.GetAll().ToList();
+            return View(classes);
+        }
+
+        // GET: Classes/Details/{id}
+        [HttpGet("{id}")]
+        public IActionResult Details(int id)
+        {
+            var cls = _classRepository.Get(id);
+            if (cls == null)
+            {
+                return NotFound();
+            }
+            return View(cls);
+        }
+
+        // Additional actions like Create, Edit, Delete can be added if needed
     }
 }
