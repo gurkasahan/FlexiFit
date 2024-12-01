ed8c1ffee82e386da06ce6403b58945ad72e1170
gurkasahan <gurk.asahan@gmail.com>
2024-12-01 14:37:57 -0500

    working on controller

--- FlexiFit/Controllers/MembersController.cs
+++ FlexiFit/Controllers/MembersController.cs
@@ -1,12 +1,151 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// FlexiFit/Controllers/MembersController.cs
+using Microsoft.AspNetCore.Mvc;
+using FlexiFit.Entities;
+using FlexiFit.Services;
+using System;
+using System.Linq;
+using FlexiFit.Entities.Members.cs;
+using FlexiFit.Entities.Models;
 
 namespace FlexiFit.Controllers
 {
+    /// <summary>
+    /// Author: Alfred, Gurkaranjit, Kamaldeep
+    /// Manages member-related operations.
+    /// </summary>
+    [Route("[controller]/[action]")]
     public class MembersController : Controller
     {
-        public IActionResult Index()
+        private readonly IRepository<Member> _memberRepository;
+        private readonly IRepository<Class> _classRepository;
+        private readonly IRepository<Workout> _workoutRepository;
+        private readonly IRepository<Booking> _bookingRepository;
+
+        public MembersController(
+            IRepository<Member> memberRepository,
+            IRepository<Class> classRepository,
+            IRepository<Workout> workoutRepository,
+            IRepository<Booking> bookingRepository)
+        {
+            _memberRepository = memberRepository;
+            _classRepository = classRepository;
+            _workoutRepository = workoutRepository;
+            _bookingRepository = bookingRepository;
+        }
+
+        // GET: Members/SignUp
+        [HttpGet]
+        public IActionResult SignUp()
         {
             return View();
+        }
+
+        // POST: Members/SignUp
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignUp(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                _memberRepository.Add(member);
+                return RedirectToAction("MemberInfo", new { id = member.MemberId });
+            }
+            return View(member);
+        }
+
+        // GET: Members/MemberInfo/{id}
+        [HttpGet("{id}")]
+        public IActionResult MemberInfo(int id)
+        {
+            var member = _memberRepository.Get(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+        // GET: Members/ManageMembership/{id}
+        [HttpGet("{id}")]
+        public IActionResult ManageMembership(int id)
+        {
+            var member = _memberRepository.Get(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/ManageMembership/{id}
+        [HttpPost("{id}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ManageMembership(int id, bool isActive)
+        {
+            var member = _memberRepository.Get(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            member.IsActive = isActive;
+            _memberRepository.Update(member);
+            return RedirectToAction("MemberInfo", new { id = member.MemberId });
+        }
+
+        // GET: Members/FindExercise
+        [HttpGet]
+        public IActionResult FindExercise()
+        {
+            var muscleGroups = _workoutRepository.GetAll()
+                .Select(w => w.MuscleGroup)
+                .Distinct()
+                .ToList();
+            return View(muscleGroups);
+        }
+
+        // GET: Members/WorkoutDetails?muscleGroup=Chest
+        [HttpGet]
+        public IActionResult WorkoutDetails(string muscleGroup)
+        {
+            var workouts = _workoutRepository.GetAll()
+                .Where(w => w.MuscleGroup == muscleGroup)
+                .ToList();
+            return View(workouts);
+        }
+
+        // GET: Members/BookClasses
+        [HttpGet]
+        public IActionResult BookClasses()
+        {
+            var classes = _classRepository.GetAll();
+            return View(classes);
+        }
+
+        // POST: Members/BookClasses
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult BookClasses(int memberId, int classId, DateTime bookingDate, TimeSpan bookingTime)
+        {
+            var booking = new Booking
+            {
+                MemberId = memberId,
+                ClassId = classId,
+                BookingDate = bookingDate,
+                BookingTime = bookingTime
+            };
+            _bookingRepository.Add(booking);
+            return RedirectToAction("ClassSchedule", new { id = memberId });
+        }
+
+        // GET: Members/ClassSchedule/{id}
+        [HttpGet("{id}")]
+        public IActionResult ClassSchedule(int id)
+        {
+            var bookings = _bookingRepository.GetAll()
+                .Where(b => b.MemberId == id)
+                .ToList();
+            return View(bookings);
         }
     }
 }
