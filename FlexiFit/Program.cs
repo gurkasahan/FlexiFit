c3cd30acf7b5b8e0bdf5bc96a830a5dafed91619
gurk.asahan@gmail.com <gurk.asahan@gmail.com>
2024-11-30 19:54:06 -0500

    Setting up Project

--- FlexiFit/Program.cs
+++ FlexiFit/Program.cs
@@ -1,5 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+//Group Members:
+//Gurkaranjit Asahan 991704979
+//Alfed Adenekan 991688457
+//Kamal Singh 991690092
+
+//Group Name: Three Headed Goat
+
+//Project Name: FlexiFit
+
+//Description: Gym Membership system where members can sign up, manage their memberships and book classes that are being offered.
+//What makes our project unique is that we will offer home workouts within our app.
+//It will not only be a gym membership app but also implement workouts within the app.
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
